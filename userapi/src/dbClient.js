3273dadb801939ab4c495f0cf703f5ae66ad475c
ELizavetaDanilina <elizaveta.danilina@edu.dsti.institute>
2024-12-11 17:52:52 +0300

    Add Docker files

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -3,15 +3,25 @@ const configure = require('./configure')
 
 const config = configure()
 var db = redis.createClient({
-  host: config.redis.host,
-  port: config.redis.port,
+  host: config.redis.host || 'redis', 
+  port: config.redis.port || 6379,    
   retry_strategy: () => {
     return new Error("Retry time exhausted")
   }
-})
+});
+
+
+db.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
+db.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
 
 process.on('SIGINT', function() {
   db.quit();
 });
 
-module.exports = db
+module.exports = db;
