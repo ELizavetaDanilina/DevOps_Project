c1b7c73b728c9718a31aae648c53b4fc45090804
ELizavetaDanilina <elizaveta.danilina@edu.dsti.institute>
2025-01-01 15:25:34 +0300

    Update dbClient.js

--- userapi/src/dbClient.js
+++ userapi/src/dbClient.js
@@ -1,6 +1,7 @@
 var redis = require("redis");
 const configure = require('./configure')
 
+// Create a Redis client instance
 const config = configure()
 var db = redis.createClient({
   host: config.redis.host || 'redis', 
@@ -10,16 +11,17 @@ var db = redis.createClient({
   }
 });
 
-
+// Event listener for Redis errors
 db.on('error', (err) => {
   console.error('Redis error:', err);
 });
 
+// Event listener for successful connection to Redis
 db.on('connect', () => {
   console.log('Connected to Redis');
 });
 
-
+// Handle graceful shutdown (SIGINT - Ctrl+C)
 process.on('SIGINT', function() {
   db.quit();
 });
